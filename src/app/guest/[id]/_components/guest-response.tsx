bd9428598e000578625eae86180daaa77d3c9250
Kyle Scudder <kyle@kylescudder.co.uk>
2024-04-27 17:04:13 +0100

    refactor(details): moved details to own component

--- src/app/guest/[id]/_components/guest-response.tsx
+++ src/app/guest/[id]/_components/guest-response.tsx
@@ -29,24 +29,8 @@ export function GuestResponse(props: { guestData: Guest[] }) {
           <Section id="timings" className="bg-background">
             <Timings />
           </Section>
-          <Section id="details" className="bg-primary pb-10 text-background">
-            <p className="text-3xl pt-14">details</p>
-            <div className="text-lg">
-              <p className="pt-10">
-                here are some of the finer details of the day
-              </p>
-              <p className="pt-10">
-                we would like our special day to be an adult only occasion.
-              </p>
-              <p>
-                we hope you can join us in celerbrating our marriage! Please
-                know that all we really want for our wedding day is for you to
-                be there to celebrate with us, but if you would like to give us
-                something, a contribution towards *blank* would be really
-                appreciated.
-              </p>
-              <p className="pt-10">dress code: Formal (Black Tie optional)</p>
-            </div>
+          <Section id="details" className="bg-primary pb-10">
+            <Details />
           </Section>
         </>
       )}
