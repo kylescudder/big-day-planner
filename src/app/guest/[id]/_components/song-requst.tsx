8a68c8eb5e6f70b7497cd0a9e56552ef49e00a22
Kyle Scudder <kyle@kylescudder.co.uk>
2024-05-17 17:51:12 +0100

    fix(style): minor style changes

--- src/app/guest/[id]/_components/song-requst.tsx
+++ src/app/guest/[id]/_components/song-requst.tsx
@@ -5,7 +5,7 @@ export function SongRequest(props: { guestData: Guest[] }) {
   return (
     <div className='text-white'>
       <p className='text-3xl pt-14'>song request</p>
-      <p className='text-lg pt-10'>
+      <p className='text-lg py-10'>
         we wanna make sure there&apos;s at least one song you just can&apos;t
         help but dance to!
       </p>
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -7,7 +7,7 @@ function Section({
   return (
     <div
       id={props.id}
-      className={cn('rounded-t-4xl w-full -mt-10 text-5xl h-fit ', className)}
+      className={cn('rounded-t-4xl w-full -mt-10 h-fit ', className)}
     >
       <article className='mb-12 px-6'>{props.children}</article>
     </div>
