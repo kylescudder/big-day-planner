37442da279692b24c81db3fe76c20eeb07c7b616
Kyle Scudder <kyle@kylescudder.co.uk>
2024-12-07 01:39:06 +0000

    feat(icon): add favicon to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+      </head>
       <body className={`${cooperBlack.className}`}>
         <ClerkProvider dynamic>
           <main className={`${cooperBlack.className} justify-center h-screen`}>
