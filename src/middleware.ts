4c5e10a9def4ff1c572c4a325aaac820da476b64
Kyle Scudder <kyle@kylescudder.co.uk>
2025-02-09 18:35:34 +0000

    fix(clerk): update auth matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,12 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isGuestsRoute = createRouteMatcher(['/guests(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  // Restrict admin route to users with specific role
   if (isGuestsRoute(req)) await auth.protect()
 })
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)'
+  ]
 }
