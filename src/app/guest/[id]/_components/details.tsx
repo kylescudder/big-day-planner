40dda6686523f58b37b7aee2c165d50edb29e424
Kyle Scudder <kyle@kylescudder.co.uk>
2025-03-03 08:44:17 +0000

    feat(details): details text only shows if no empty

--- src/app/guest/[id]/_components/details.tsx
+++ src/app/guest/[id]/_components/details.tsx
@@ -4,10 +4,26 @@ export function Details(props: { details: Detail }) {
   return (
     <div>
       <p className='text-5xl pt-14'>details</p>
-      <div className='grid gap-4 mt-10 p-2 border-primary border-4 rounded-2xl'>
+      <div
+        className={`grid gap-4 mt-10 p-2 border-primary border-4 rounded-2xl ${props.details.detailsTextSubheader ? '' : 'hidden'}`}
+      >
         <p className='pt-10'>{props.details.detailsTextSubheader}</p>
-        <p className='pt-10'>{props.details.adultsOnlyText}</p>
+      </div>
+      {props.details.adultsOnly ?? (
+        <div
+          className={`grid gap-4 mt-10 p-2 border-primary border-4 rounded-2xl ${props.details.adultsOnlyText ? '' : 'hidden'}`}
+        >
+          <p className='pt-10'>{props.details.adultsOnlyText}</p>
+        </div>
+      )}
+      <div
+        className={`grid gap-4 mt-10 p-2 border-primary border-4 rounded-2xl ${props.details.detailsText ? '' : 'hidden'}`}
+      >
         <p className='pt-10'>{props.details.detailsText}</p>
+      </div>
+      <div
+        className={`grid gap-4 mt-10 p-2 border-primary border-4 rounded-2xl ${props.details.dresscode ? '' : 'hidden'}`}
+      >
         <p className='pt-10'>{props.details.dresscode}</p>
       </div>
     </div>
