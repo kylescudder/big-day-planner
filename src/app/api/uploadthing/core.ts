e763798361aa1535cff03304c7cc6fc5dee83b7a
Kyle Scudder <kyle@kylescudder.co.uk>
2025-03-10 07:45:32 +0000

    refactor(uploadthing): removed unused imports and comments

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,41 +1,27 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { UploadThingError } from 'uploadthing/server'
 import { auth } from '@clerk/nextjs/server'
-import { updateImageRecord } from '@/server/service'
-import { ImageType } from '@/consts/image-types'
-import { Image } from '@/server/db/schema'
-import { uuidv4 } from '@/lib/utils'
 
 const f = createUploadthing()
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
     image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
       maxFileSize: '4MB',
       maxFileCount: 1
     }
   })
-    // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       const user = await auth()
       if (!user) throw new UploadThingError('Unauthorized')
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.userId }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
       console.log('Upload complete for userId:', metadata.userId)
 
       console.log('file url', file.url)
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId }
     }),
   logoUploader: f({
