da4e7e4efac837af8f02191fddfa4b1d8330ae9d
Kyle Scudder <kyle@kylescudder.co.uk>
2025-07-19 11:38:07 +0100

    feat(template): rsvp thanks template update to be less specific

--- src/components/templates/emails/rsvp-thanks-template.tsx
+++ src/components/templates/emails/rsvp-thanks-template.tsx
@@ -16,9 +16,8 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   <div>
     <p>
       Hey {forename}! Thank you for submitting your RSVP.
-      {rsvpAnswer === true
-        ? `\nWe are so excited to see you there! If you haven\' done so already, now is a great time to submit your song choice and let us know if any dietary requirements.
-        ${bride} and ${groom} are so excited to see you there!`
+      {rsvpAnswer
+        ? `${bride} and ${groom} are so excited to see you there!`
         : "\nWe are really sorry that you aren't going to be there to celebrate with us, but look forward to seeing you soon for a catch up."}
     </p>
   </div>
